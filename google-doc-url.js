3d390c34968ab27f094ba953f10205b658b91ec5
Denisas37 <denisvatincev@gmail.com>
2024-12-03 14:31:10 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSPScBwdAlVIEXxcOX7PU8zPWxahhGbuWBqseKAZ4-b4REpJpcsdqCzlsa0VzPRDch_72bivR9QLhhC/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1dJvoSz0rAMBOFkzeB-OYt3FpISS-hEQL1a75uJoXcVQ/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
